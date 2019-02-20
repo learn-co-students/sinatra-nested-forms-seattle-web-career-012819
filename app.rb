f38256b6b99d599963ee891c4e7114f83cddb4ee
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-02-20 09:00:27 -0800

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,21 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      erb :show
+    end
 
   end
 end
