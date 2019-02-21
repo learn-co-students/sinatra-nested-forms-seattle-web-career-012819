96edaa7285542c105a6de87836e0c313e6b8a18b
github username <github email address>
2019-02-21 08:43:17 -0800

    Done.

--- app.rb
+++ app.rb
@@ -18,14 +18,17 @@ module FormsLab
     # end
 
     post '/pirates' do
-      @pirate_args = params[:pirate]
-      @ship_hash = params[:pirate][:ships]
-      @pirate = Pirate.new(@pirate_args)
-      @ship_hash.each do |ship_args|
+      Pirate.clear
+      Ship.clear
+      pirate_args = params[:pirate]
+      ship_hash = params[:pirate][:ships]
+      Pirate.new(pirate_args)
+      ship_hash.each do |ship_args|
         Ship.new(ship_args)
       end
       @ship1 = Ship.all[0]
       @ship2 = Ship.all[1]
+      @pirate = Pirate.all[0]
       erb :show
     end
   end
