88cab9781e0e306c4bfe30f0e1c0c7c00fa35850
github username <crishanks12@gmail.com>
2019-02-20 16:19:47 -0800

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,37 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    set :views, 'views/pirates'
+
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    get '/pirates' do 
+      erb :show
+    end
+
+    post '/pirates' do
+      
+      name = params['pirate']['name']
+      weight = params['pirate']['weight']
+      height = params['pirate']['height']
+      @pirate = Pirate.new(name, weight, height)
+      
+      params[:pirate][:ships].each do |ship_hash|
+        name = ship_hash['name']
+        type = ship_hash['type']
+        booty = ship_hash['booty']
+        Ship.new(name, type, booty)
+      end
+
+      @ships = Ship.all
+      erb :show
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+    PIRATES = []
+
+    attr_reader :name, :weight, :height
+    def initialize(name, weight, height)
+        @name = name
+        @weight = weight
+        @height = height
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
 end
